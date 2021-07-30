929e96a733f9cb7772627e2feed8bb2b4441e29d
Qusay114 <eng.qusay015@gmail.com>
2021-07-30 19:05:56 +0300

    create userAccount and requests

---
+++ src/main/java/com/sourcecode/controllers/AccountUserController.java
@@ -0,0 +1,43 @@
+package com.sourcecode.controllers;
+
+import com.sourcecode.infrastructure.services.UserAccountService;
+import com.sourcecode.models.UserAccount;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.ArrayList;
+
+@Controller
+public class AccountUserController {
+
+    @Autowired
+    private UserAccountService userAccountService ;
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder ;
+
+    @PostMapping("/signup")
+    public ResponseEntity<UserAccount> createUserAccount(@RequestBody UserAccount userAccount){
+        userAccount.setPassword(passwordEncoder.encode(userAccount.getPassword()));
+        userAccount = userAccountService.createUserAccount(userAccount);
+        Authentication authentication = new UsernamePasswordAuthenticationToken(userAccount , null , new ArrayList<>());
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        return new ResponseEntity<UserAccount>(userAccount , HttpStatus.CREATED) ;
+    }
+
+    @GetMapping("/profile")
+    public ResponseEntity<UserAccount> getUserAccount(){
+        UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        UserAccount userAccount = userAccountService.findUserAccount(userDetails.getUsername());
+        return new ResponseEntity<>(userAccount , HttpStatus.ACCEPTED) ;
+    }
+}
--- src/main/java/com/sourcecode/security/UserDetailsServiceImpl.java
+++ src/main/java/com/sourcecode/security/UserDetailsServiceImpl.java
@@ -1,6 +1,6 @@
 package com.sourcecode.security;
 
-import com.sourcecode.repository.AccountRepository;
+import com.sourcecode.infrastructure.UserAccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -11,10 +11,10 @@ import org.springframework.stereotype.Service;
 public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Autowired
-    AccountRepository accountRepository;
+    private UserAccountRepository userAccountRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return accountRepository.findByUsername(username);
+        return userAccountRepository.findUserAccountByUsername(username);
     }
 }
