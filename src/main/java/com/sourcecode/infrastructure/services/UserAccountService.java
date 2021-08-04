0ec78e6d2f4e465933029aaed80f2bc8f66d0d50
mariamodat <mariamodat0@gmail.com>
2021-08-04 12:52:56 +0300

    added both about us page as well as Q&A

--- src/main/java/com/sourcecode/infrastructure/services/UserAccountService.java
+++ src/main/java/com/sourcecode/infrastructure/services/UserAccountService.java
@@ -2,9 +2,13 @@ package com.sourcecode.infrastructure.services;
 
 import com.sourcecode.models.UserAccount;
 
+import java.util.List;
+
 public interface UserAccountService {
     UserAccount createUserAccount(UserAccount userAccount);
     UserAccount findUserAccount(String username);
     UserAccount findUserAccount(Long id);
     UserAccount deleteUserAccount(String username);
+
+    List<UserAccount> findAllUsers();
 }
