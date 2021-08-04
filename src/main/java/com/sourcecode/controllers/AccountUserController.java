0413ad31b345153c72457a48866eac3bf2d59c62
LayanaBaba <leana_baba@yahoo.com>
2021-08-04 17:29:55 +0300

    merge

--- src/main/java/com/sourcecode/controllers/AccountUserController.java
+++ src/main/java/com/sourcecode/controllers/AccountUserController.java
@@ -15,7 +15,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.security.Principal;
